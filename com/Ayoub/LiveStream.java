8e9af1519e25d486692a9ceb002c35259b04cf07
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-04-25 12:27:15 +0200

    Added getter and setter

--- com/Ayoub/LiveStream.java
+++ com/Ayoub/LiveStream.java
@@ -1,5 +1,14 @@
 package com.Ayoub;
 
+// arena matches will be livestreamed on one main streaming channel
 public class LiveStream {
     private String streamName;
+
+    public String getStreamName() {
+        return streamName;
+    }
+
+    public void setStreamName(String streamName) {
+        this.streamName = streamName;
+    }
 }
