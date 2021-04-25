b5afd0c9190b799f03d97843f106d68f18694f5f
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-04-25 15:21:01 +0200

    Added constructor

--- com/Ayoub/LiveStream.java
+++ com/Ayoub/LiveStream.java
@@ -1,14 +1,18 @@
 package com.Ayoub;
 
 // arena matches will be livestreamed on one main streaming channel
-public class LiveStream {
+public class LiveStream implements Streaming {
     private String streamName;
+
+    public LiveStream(String sn) {
+        this.streamName = sn;
+    }
 
     public String getStreamName() {
         return streamName;
     }
 
-    public void setStreamName(String streamName) {
-        this.streamName = streamName;
+    public void setStreamName(String sn) {
+        this.streamName = sn;
     }
 }
