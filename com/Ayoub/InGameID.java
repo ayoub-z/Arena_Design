f93b09596c17dbd55b076685a8712726c170e44c
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-04-25 12:19:11 +0200

    Added constructor

--- com/Ayoub/InGameID.java
+++ com/Ayoub/InGameID.java
@@ -3,6 +3,11 @@ package com.Ayoub;
 public class InGameID {
     private String id;
 
+    // each player is part of an "account" and this account has an id
+    public InGameID(String id) {
+        this.id = id;
+    }
+
     public String getId() {
         return id;
     }
