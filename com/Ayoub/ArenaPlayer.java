d2e82f53f7a154292e40e66664bd4371b3fefc09
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-04-25 15:20:33 +0200

    Added extra getter to interface

--- com/Ayoub/ArenaPlayer.java
+++ com/Ayoub/ArenaPlayer.java
@@ -2,6 +2,7 @@ package com.Ayoub;
 
 public interface ArenaPlayer {
     String getName();
+    String getClassType();
     String getSpecType();
     int getRating();
     boolean compareRating(int rating1, int rating2);
