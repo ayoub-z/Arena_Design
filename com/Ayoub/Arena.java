92f74ff78265289a0069ff9b4252435f5eb3932b
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-04-25 17:24:43 +0200

    Updated toString and added new interface variables

--- com/Ayoub/Arena.java
+++ com/Ayoub/Arena.java
@@ -1,21 +1,23 @@
 package com.Ayoub;
 
-public class Arena implements Streaming{
+public class Arena implements Cast{
     private final ArenaPlayer arenaPlayer1;
     private final ArenaPlayer arenaPlayer2;
-    private final Streaming liveStream;
+    private final Cast liveStream;
+    private final Cast cast;
 
-    public Arena(ArenaPlayer arenaPlayer1, ArenaPlayer arenaPlayer2, Streaming liveStream) throws Exception {
+    public Arena(ArenaPlayer arenaPlayer1, ArenaPlayer arenaPlayer2, Cast liveStream, Cast cast) throws Exception {
         // Players with the same specialization type cannot play against each other, unless they are both dps.
-        if ( arenaPlayer1.getSpecType().equals(arenaPlayer2.getSpecType()) && !arenaPlayer1.getSpecType().equals("Dps")) {
+        if (arenaPlayer1.getSpecType().equals(arenaPlayer2.getSpecType()) && !arenaPlayer1.getSpecType().equals("Dps")) {
             throw new Exception(arenaPlayer1.getName() + " and " + arenaPlayer2.getName() + " can't both be a " + arenaPlayer1.getSpecType());
         }
-        if (arenaPlayer1.compareRating(arenaPlayer1.getRating(), arenaPlayer2.getRating())) {
+        if (!arenaPlayer1.compareRating(arenaPlayer1.getRating(), arenaPlayer2.getRating())) {
             throw new Exception("The rating of " + arenaPlayer1.getName() + " and " + arenaPlayer2.getName() + " is too far apart");
         }
         this.arenaPlayer1 = arenaPlayer1;
         this.arenaPlayer2 = arenaPlayer2;
         this.liveStream = liveStream;
+        this.cast = cast;
     }
 
     public ArenaPlayer getArenaPlayer1() {
@@ -34,8 +36,13 @@ public class Arena implements Streaming{
         return liveStream.getStreamName();
     }
 
+    public String getName() {
+        return cast.getName();
+    }
+
     public void setLiveStream(String ls){
     }
+
 
     public String toString(){
         return "\nThere are 2 fighters in this match: \n------------------------------------" +
@@ -43,7 +50,8 @@ public class Arena implements Streaming{
                 getArenaPlayer1().getClassType() + "\n\tRating: " + getArenaPlayer1().getRating() + "\n\nPlayer 2: " +
                 getArenaPlayer2().getName() + "\n\tRole: " + getArenaPlayer2().getSpecType() + "\n\tClass: " +
                 getArenaPlayer2().getClassType() + "\n\tRating: " + getArenaPlayer2().getRating() +
-                "\n These matches will be streamed on: " + getStreamName();
+                "\n\nThese matches will be streamed on: " + getStreamName() + "\nwith the commentator being: "
+                + getName() + " of the official World of Warcraft team!";
     }
 
 
