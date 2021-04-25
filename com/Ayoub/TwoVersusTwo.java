79bdcbc55b5127913145dc550201b2e8f60cdd26
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-04-25 12:23:22 +0200

    Filled compareRating func and updated getters

--- com/Ayoub/TwoVersusTwo.java
+++ com/Ayoub/TwoVersusTwo.java
@@ -1,15 +1,31 @@
 package com.Ayoub;
 
-public class TwoVersusTwo {
-    private PlayerCharacter player;
+public class TwoVersusTwo implements ArenaPlayer{
+    private final PlayerCharacter player1;
+    private final PlayerCharacter player2;
 
-    public TwoVersusTwo() {
+    public TwoVersusTwo(PlayerCharacter player1,PlayerCharacter player2){
+        this.player1 = player1;
+        this.player2 = player2;
     }
 
     public String getName() {
-        return player.getName();
+        return player1.getName() + "/" + player2.getName();
     }
+
+    public int getRating() {
+        return (player1.getRating() + player2.getRating())/2 ;
+    }
+
+
+    public String getSpecType() {
+        return player1.getSpecType() + "/" + player2.getSpecType();
+    }
+
     public boolean compareRating(int rating1, int rating2) {
-        return false;
+        rating1 = player1.getRating();
+        rating2 = player2.getRating();
+        return player1.compareRating(rating1, rating2);
+
     }
 }
