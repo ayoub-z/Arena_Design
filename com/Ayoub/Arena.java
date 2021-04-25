0792007eb3451502690aa647b780f97d2e1ffd4c
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-04-25 12:21:19 +0200

    Filled in constructor and getPlayer function

--- com/Ayoub/Arena.java
+++ com/Ayoub/Arena.java
@@ -1,16 +1,29 @@
 package com.Ayoub;
 
 public class Arena {
-    private ArenaPlayer arenaPlayer1;
-    private ArenaPlayer arenaPlayer2;
-    private PlayerCharacter player;
+    private final ArenaPlayer arenaPlayer1;
+    private final ArenaPlayer arenaPlayer2;
     private LiveStream liveStream;
 
-    public Arena() {
+    public Arena(ArenaPlayer arenaPlayer1, ArenaPlayer arenaPlayer2) throws Exception {
+        // Players with the same specialization type cannot play against each other, unless they are both dps.
+        if ( arenaPlayer1.getSpecType().equals(arenaPlayer2.getSpecType()) && !arenaPlayer1.getSpecType().equals("Dps")) {
+            throw new Exception("The spec type of player 1 cannot be the same as that of player2, unless if they are both damage dealers");
+        }
+        this.arenaPlayer1 = arenaPlayer1;
+        this.arenaPlayer2 = arenaPlayer2;
     }
 
-    public String getPlayer() {
-        return player.getSpecType();
+    public ArenaPlayer getArenaPlayer1() {
+        return arenaPlayer1;
+    }
+
+    public ArenaPlayer getArenaPlayer2() {
+        return arenaPlayer2;
+    }
+
+    public String getSpecType() {
+        return arenaPlayer1.getSpecType() + "/" + arenaPlayer2.getSpecType();
     }
 
     public LiveStream getLiveStream() {
