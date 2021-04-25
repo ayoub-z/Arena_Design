15b30f9843ee5338ec020585863714fffd3fb620
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-04-25 19:57:26 +0200

    Added comment

--- com/Ayoub/InGameId.java
+++ com/Ayoub/InGameId.java
@@ -4,6 +4,7 @@ public class InGameId {
     private String id;
 
     // each player is part of an "account" and this account always has an id
+    // each account can have a maximum of 10 player characters
     public InGameId(String id) {
         this.id = id;
     }
